e260d07b28f059630cac1347c867563a20dc5b26
divyanshkumar99 <divyanshkumar99bly@gmail.com>
2022-03-09 13:09:20 +0530

    My First Commit

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using WebApplication9.Models;
+
+namespace WebApplication9.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        public IActionResult Get()
+        {
+            var Emplist = new List<Employee>()
+            {
+                new Employee() {EName="Divyansh",EId=100,EDept="Development",EPno=1234567890},
+                new Employee() {EName="DKumar",EId=102,EDept="Security",EPno=1234567890},
+                new Employee() {EName="Dkumar2",EId=104,EDept="Management",EPno=1234567890},
+                new Employee() {EName="DKumar3",EId=106,EDept="Transport",EPno=1234567890},
+                new Employee() {EName="DKumar4",EId=108,EDept="FoodSupply",EPno=1234567890}
+            };
+            return Ok(Emplist);
+        }
+    }
+}
---
+++ Models/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication9.Models
+{
+    public class Employee
+    {
+        public string EName { get; set; }
+        public int EId { get; set; }
+        public string EDept { get; set; }
+        public double EPno { get; set; }
+    }
+}
